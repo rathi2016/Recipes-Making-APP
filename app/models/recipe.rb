d0f2b9b48d3bb49e13e0863e898555cc71e2a5cf
Rathi <github email address>
2017-12-22 15:40:36 -0500

    added model association inside Recipie

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,7 @@
 class Recipe < ApplicationRecord
+  has_many :ingredients
+  accepts_nested_attributes_for :ingredients, allow_destroy: true, dependent: :destroy
+
+  has_many :steps
+  accepts_nested_attributes_for :steps, allow_destroy: true, dependent: :destroy
 end
